46cf217537a356bfde3856e2814ac1c6cfa90952
George Velentzas <geovelentzas@gmail.com>
2017-12-15 20:47:01 +0200

    Remove grayshow function and greek comments

--- lab5/grayshow.m
+++
@@ -1,7 +0,0 @@
-function grayshow(I)
-
-I = double(I);
-mx = max(max(I));
-mn = min(min(I));
-I = (I - mn) ./ (mx - mn);
-imshow(I)
--- lab5/harris_laplace.m
+++ lab5/harris_laplace.m
@@ -13,25 +13,22 @@ lap = laplace(I, s_step, s_n);
 
 % keep points where laplacian attains a local maximum over scale
 points = zeros(0, 3);
-for i = 1:size(harris_pts, 1)                       %Για κάθε σημείο βρές 
-    x=harris_pts(i,1);                              %τις συντεταγμένες του x,y
-    y=harris_pts(i,2);                              %στην εικόνα και την
-    s=harris_pts(i,3);                              %κλίμακά του s 
-    if s==1                                         %Αν η κλίμακα είναι η 1 τότε
-        if lap(x,y,s)>lap(x,y,s+1)                  %έλεγξε αν η λαπλασιανή του είναι
-        points = [points ; harris_pts(i,:)];        %μεγαλύτερη απο την επόμενη κλίμακα
-        end                                         %και αν ναι τοτε βαλτο στον πινακα με τα σημεία ακμών
-    elseif s==s_n                                   %Αν η κλίμακα είναι η τελευταία 
-        if lap(x,y,s)>lap(x,y,s-1)                  %έλεγξε με την προτελευταία
-        points = [points ; harris_pts(i,:)];        %και κάνε το ίδιο    
-        end                                         %Αλλιώς έλεγξε με την προηγούμενη και την επόμενη
+for i = 1:size(harris_pts, 1)                       
+    x=harris_pts(i,1);                              
+    y=harris_pts(i,2);                              
+    s=harris_pts(i,3);                              
+    if s==1                                         
+        if lap(x,y,s)>lap(x,y,s+1)                  
+        points = [points ; harris_pts(i,:)];        
+        end                                         
+    elseif s==s_n                                    
+        if lap(x,y,s)>lap(x,y,s-1)                  
+        points = [points ; harris_pts(i,:)];            
+        end                                         
     elseif (lap(x,y,s)>lap(x,y,s-1))&&(lap(x,y,s)>lap(x,y,s+1))
-        points = [points ; harris_pts(i,:)];        %και αν είναι βαλ'το στον points
-    end                                             %Έτσι ο points θα έχει τα σημεία harris_pts
-end                                                 %που η λαπλασιανή τους παρουσιάζει τοπικό μέγιστο
-
-
-
+        points = [points ; harris_pts(i,:)];        
+    end                                             
+end                                                 
 
 
 % Set scale to 3*sigma for display
