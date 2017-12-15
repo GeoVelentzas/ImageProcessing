2835ada0efff64f9f27dd85e80309b509005ad63
George Velentzas <geovelentzas@gmail.com>
2017-12-15 20:35:47 +0200

    Add lab5 material

---
+++ lab5/lab5.m
@@ -0,0 +1,11 @@
+close all;clear all; clc;
+
+I1 = imread('building.jpg');
+I2 = imread('house2.jpg');
+points1 = harris_laplace(I1);
+points2 = harris_laplace(I2);
+figure(1);
+subplot(1,2,1);
+draw_points(I1, points1, 'Harris');
+subplot(1,2,2);
+draw_points(I2, points2, 'Harris');
