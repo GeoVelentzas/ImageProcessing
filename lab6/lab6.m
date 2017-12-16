946b5d52fd41e8e68c4fa90691c54ed3f4e113d9
George Velentzas <geovelentzas@gmail.com>
2017-12-16 15:01:42 +0200

    Add subplots for eiffel tower detection in sample images

--- lab6/lab6.m
+++ lab6/lab6.m
@@ -4,20 +4,69 @@ addpath(genpath('./'));
 
 img1 = imread('eiffel_00.jpg');
 p1 = harris(img1);
-img2 = imread('eiffel_00.jpg');
+
+img2 = imread('eiffel_01.jpg');
 p2 = harris(img2);
 
-%���������� binsize ��� threshold
+img3 = imread('eiffel_02.jpg');
+p3 = harris(img3);
+
+img4 = imread('eiffel_03.jpg');
+p4 = harris(img4);
+
 binsize = 3;
 thres = 0.9;
 
-%����� ���������� hough2 ��� ����������� r, c, A, A_t
-[r, c, A, A_t]= hough2d(img1, p1, img2, p2, binsize, thres);
+subplot(2,2,1);
+imshow(img1);
+title('reference picture');
 
-%�������� ������������� ���� ������ �� ��������� opac ������� �� �� ������
-%��� ����������� ��� ����� ������� � ������.
+[r, c, A, A_t]= hough2d(img1, p1, img2, p2, binsize, thres);
+subplot(2,2,2);
 imshow(img2);
 hold on;
 for i =1 : length(r)
 draw_bb(img1,r(i),c(i),0.4/length(r))
 end
+title('found in picture 2');
+
+
+[r, c, A, A_t]= hough2d(img1, p1, img3, p3, binsize, thres);
+subplot(2,2,3);
+imshow(img3);
+hold on;
+for i =1 : length(r)
+draw_bb(img1,r(i),c(i),0.4/length(r))
+end
+title('found in picture 3');
+
+
+[r, c, A, A_t]= hough2d(img1, p1, img4, p4, binsize, thres);
+subplot(2,2,4);
+imshow(img4);
+hold on;
+for i =1 : length(r)
+draw_bb(img1,r(i),c(i),0.4/length(r))
+end
+title('found in picture 3');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
